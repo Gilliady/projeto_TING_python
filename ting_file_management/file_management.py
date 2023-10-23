87bef6c74ce7e695a7f6ee0f19a3ce4044eb82ce
Gilliady Azevedo <gilliadyazevedo@gmail.com>
2023-10-23 16:38:45 +0000

    requisito 02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    try:
+        with open(path_file, 'r', encoding='utf-8') as file:
+            extension = file.name.split('.')[1]
+            if extension != 'txt':
+                print("Formato inválido", file=sys.stderr)
+            return file.read().split('\n')
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return None
