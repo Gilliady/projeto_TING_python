95dac49f7c2bd7e70e316bc60aee9b0fb6d26005
Gilliady Azevedo <gilliadyazevedo@gmail.com>
2023-10-24 17:17:28 +0000

    requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,27 @@
-def exists_word(word, instance):
+from ting_file_management.queue import Queue
+
+
+def exists_word(word, instance: Queue):
     """Aqui irá sua implementação"""
+    len_instance = len(instance)
+    result = []
+    i = 0
+    while i < len_instance:
+        lines_index_match = []
+        file = instance.search(i)
+        for line in file["linhas_do_arquivo"]:
+            if word.lower() in line.lower():
+                lines_index_match.append({
+                    "linha": file["linhas_do_arquivo"].index(line) + 1,
+                    })
+        if lines_index_match:
+            result.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": lines_index_match,
+            })
+        i += 1
+    return result
 
 
 def search_by_word(word, instance):
