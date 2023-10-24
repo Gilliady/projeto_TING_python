147833b1b14bc450234e4b20d6e8d1bdbfcc44ef
Gilliady Azevedo <gilliadyazevedo@gmail.com>
2023-10-24 14:48:44 +0000

    requisitos file_process.py

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,36 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+from queue import Queue
+from .file_management import txt_importer
 
 
-def remove(instance):
+def process(path_file: str, instance: Queue):
     """Aqui irá sua implementação"""
+    lines = txt_importer(path_file)
+    try:
+        output = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines
+          }
+        instance.enqueue(output)
+        print(output, file=sys.stdout)
+    except ValueError:
+        print("Arquivo já adicionado", file=sys.stderr)
+
+
+def remove(instance: Queue):
+    """Aqui irá sua implementação"""
+    try:
+        file = instance.dequeue()
+        print(f"Arquivo {file['nome_do_arquivo']} removido com sucesso",
+              file=sys.stdout)
+    except IndexError:
+        print("Não há elementos", file=sys.stdout)
 
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+    try:
+        print(instance.search(position), file=sys.stdout)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -9,9 +9,14 @@ class Queue(AbstractQueue):
         return len(self.__queue)
 
     def enqueue(self, value):
+        for i in self.__queue:
+            if i == value:
+                raise ValueError("Arquivo já adicionado")
         self.__queue.append(value)
 
     def dequeue(self):
+        if not self.__queue:
+            raise IndexError("A fila está vazia")
         return self.__queue.pop(0)
 
     def search(self, index):
